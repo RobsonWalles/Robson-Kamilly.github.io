527944768fa15e5f6942c6518b0cdaf7b3ff6e00
Robson e Kamilly <robsonwalles@gmail.com>
2024-01-30 13:51:56 -0300

    responsivo

--- js/movile-navbar.js
+++ js/movile-navbar.js
@@ -1,34 +1,29 @@
-class MobileNavbar {
-  constructor(mobileMenu, navList, navLinks) {
-    this.mobileMenu = document.querySelector(mobileMenu);
-    this.navList = document.querySelector(navList);
-    this.navLinks = document.querySelectorAll(navLinks);
-    this.activeClass = "active";
+// Set the date we're counting down to
+var countDownDate = new Date("Jun 9, 2024 15:00:00").getTime();
 
-    this.handleClick = this.handleClick.bind(this);
+// Update the count down every 1 second
+var x = setInterval(function () {
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("dia").innerHTML = days + "d";
+  document.getElementById("hora").innerHTML = hours + "h";
+  document.getElementById("minuto").innerHTML = minutes + "m";
+  document.getElementById("segundo").innerHTML = seconds + "s";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
   }
-
-  handleClick() {
-    console.log(this);
-    this.navList.classList.toggle(this.activeClass);
-  }
-
-  addClickEvent() {
-    this.mobileMenu.addEventListener("click", this.handleClick);
-  }
-
-  init() {
-    if (this.mobileMenu) {
-      this.addClickEvent();
-    }
-    return this;
-  }
-}
-
-const mobileNavbar = new MobileNavbar(
-  ".mobile-menu",
-  ".nav-list",
-  ".nav-list li"
-);
-
-mobileNavbar.init();
+}, 1000);
