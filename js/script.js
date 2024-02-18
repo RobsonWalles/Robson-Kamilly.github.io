3fa4f91d75ca5a2ad86f66162bf9170977a35202
Robson e Kamilly <robsonwalles@gmail.com>
2024-02-18 10:17:02 -0300

    adicionado pix

--- js/script.js
+++ js/script.js
@@ -9,3 +9,15 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+// Função para abrir o popup
+function openPopup() {
+  var popup = document.getElementById("popup");
+  popup.style.display = "block";
+}
+
+// Função para fechar o popup
+function closePopup() {
+  var popup = document.getElementById("popup");
+  popup.style.display = "none";
+}
